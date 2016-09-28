06bb3a04c37afbd0d137ef07c12465bb8b83edb9
cam-shotter <cameronshotter@gmail.com>
2016-09-29 11:12:55 +1300

    added login button

--- components/Login.js
+++ components/Login.js
@@ -21,9 +21,9 @@ class Login extends Component {
           placeholder="Github Username" />
         <TextInput style={styles.input}
           placeholder="Github Password"
-          secureTextEntry="true" />
+          secureTextEntry={true} />
         <TouchableHighlight style={styles.button} >
-          <Text style={styles.button} >
+          <Text style={styles.buttonText} >
             Log in
           </Text>
         </TouchableHighlight>
@@ -53,7 +53,19 @@ const styles = StyleSheet.create ({
     padding: 4,
     fontSize: 18,
     borderWidth: 1,
-    borderColor: '#48bbec'
+    borderColor: '#48BBEC'
+  },
+  button: {
+    height: 50,
+    backgroundColor: '#48BBEC',
+    alignSelf: 'stretch',
+    marginTop: 10,
+    justifyContent: 'center'
+  },
+  buttonText: {
+    fontSize: 22,
+    color: '#FFF',
+    alignSelf: 'center'
   }
 })
 
